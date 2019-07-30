1262332415d2408e15c17f6f327c52871f50a93f
DimaH8 <dima.huzei@gmail.com>
2019-07-30 16:00:31 +0300

    Start LabStack

---
+++ Lab1Stack/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "stack.h"
+using namespace std;
+
+int main()
+{
+	create_stack();
+	system("pause");
+	return 0;
+}
---
+++ Lab1Stack/stack.cpp
@@ -0,0 +1,7 @@
+#include "stack.h"
+#include <iostream>
+using namespace std;
+void create_stack()
+{
+	cout << "stack created. My congratulations!" << endl;
+}
